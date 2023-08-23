188ee319b4120bc5e026ced70440be44a152f681
Kulpot <kulpotwahu@gmail.com>
2023-08-23 17:51:17 +0800

    Add project files.

---
+++ Reference Types Example 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=VSKxZIxacEw&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=5
+//  object graph -- 
+
+class Cow
+{
+    int mooCount;
+    Udder udder;
+    public Cow()
+    {
+        udder = new Udder();
+    }
+}
+
+class Udder
+{
+    int numTeets;
+    double ouncesOfMilk;
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        Cow c = new Cow();
+    }
+}
